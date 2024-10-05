b667b43de3dfa2fa78fc6a4baf24af570acf69d6
iamminseongKim <minseongkimim@gmail.com>
2024-10-05 21:20:36 +0900

    5. 객체 지향 적용하기 - Value Object - Cell의 상태를 value object로 만들기.

--- src/main/java/cleancode/minesweeper/tobe/BoardIndexConverter.java
+++ src/main/java/cleancode/minesweeper/tobe/BoardIndexConverter.java
@@ -4,27 +4,27 @@ public class BoardIndexConverter {
 
     private static final char BASE_CHAR_FOR_COL = 'a';
 
-    public int getSelectedRowIndex(String cellInput, int rowSize) {
+    public int getSelectedRowIndex(String cellInput) {
         String cellInputRow = cellInput.substring(1);
-        return convertRowFrom(cellInputRow, rowSize);
+        return convertRowFrom(cellInputRow);
     }
 
-    public int getSelectedColIndex(String cellInput, int colSize) {
+    public int getSelectedColIndex(String cellInput) {
         char cellInputCol = cellInput.charAt(0);
-        return convertColFrom(cellInputCol, colSize); // 전치사로 메서드명을 지어서 안에 파라미터랑 의미를 이어가서 읽게 만듬.
+        return convertColFrom(cellInputCol); // 전치사로 메서드명을 지어서 안에 파라미터랑 의미를 이어가서 읽게 만듬.
     }
 
-    private int convertRowFrom(String cellInputRow, int rowSize) { // "10"
+    private int convertRowFrom(String cellInputRow) { // "10"
         int rowIndex = Integer.parseInt(cellInputRow) - 1;
-        if (rowIndex < 0 || rowIndex >= rowSize) {
+        if (rowIndex < 0) {
             throw new GameException("잘못된 입력 입니다.");
         }
         return rowIndex;
     }
 
-    private int convertColFrom(char cellInputCol, int colSize) { // 'a'
+    private int convertColFrom(char cellInputCol) { // 'a'
         int colIndex = cellInputCol - BASE_CHAR_FOR_COL;
-        if (colIndex < 0 || colIndex >= colSize) {
+        if (colIndex < 0) {
             throw new GameException("잘못된 입력 입니다.");
         }
         return colIndex;
