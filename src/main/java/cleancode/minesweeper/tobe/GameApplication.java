582199fa6907cc470139bcb8868aad9678f1e8c8
iamminseongKim <minseongkimim@gmail.com>
2024-10-05 17:37:56 +0900

    4. 객체 지향 패러다임 - DIP - OutputHandler 의 용어 정리 - show, print (추상화 했기 때문에 이제 정리가 필요)

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -21,4 +21,18 @@ public class GameApplication {
         minesweeper.initialize();
         minesweeper.run();
     }
+
+    /**
+     * DIP (Dependency Inversion Principle)
+     *  - 고수준 모듈이 저수준 모듈을 참조하것 : 순방향
+     *  - 고수준, 저수준 모듈이 모두 추상화에 의존해야 하는 것 : 역방향
+     * DI (Dependency Injection)
+     *  - 의존성 주입 (스프링)
+     *  - "3"
+     *  - 제 3자(스프링 컨텍스트) 가 나와 너의 의존성을 주입시켜준다.
+     * IOC (Inversion of Control)
+     *  - 제어의 역전 (역방향)
+     *  - 프로그램의 흐름을 개발자가 아닌 프레임워크가 제어하도록 한다.
+     *  - 제어가 순방향이라면 개발자가 프로그램의 흐름을 제어해야 한다.
+     */
 }
--- src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
+++ src/main/java/cleancode/minesweeper/tobe/Minesweeper.java
@@ -37,11 +37,11 @@ public class Minesweeper implements GameInitializable, GameRunnable {
                 outputHandler.showBoard(gameBoard);
 
                 if (doseUserWinTheGame()) {
-                    outputHandler.printGameWinningComment();
+                    outputHandler.showGameWinningComment();
                     break;
                 }
                 if (doseUserLoseTheGame()) {
-                    outputHandler.printGameLosingComment();
+                    outputHandler.showGameLosingComment();
                     break;
                 }
 
@@ -49,9 +49,9 @@ public class Minesweeper implements GameInitializable, GameRunnable {
                 String userActionInput = getUserActionInputFromUser();
                 actOnCell(cellInput, userActionInput);
             } catch (GameException e) {
-                outputHandler.printExceptionMessage(e);
+                outputHandler.showExceptionMessage(e);
             } catch (Exception e) {
-                outputHandler.printSimpleMessage("프로그램에 문제가 생겼습니다..");
+                outputHandler.showSimpleMessage("프로그램에 문제가 생겼습니다..");
             }
 
         }
@@ -99,12 +99,12 @@ public class Minesweeper implements GameInitializable, GameRunnable {
 
     private String getUserActionInputFromUser() {
 
-        outputHandler.printCommentForUserAction();
+        outputHandler.showCommentForUserAction();
         return inputHandler.getUserInput();
     }
 
     private String getCellInputFromUser() {
-        outputHandler.printCommentForSelectingCell();
+        outputHandler.showCommentForSelectingCell();
         return inputHandler.getUserInput();
     }
 
