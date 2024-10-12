716b682d2ea7149448adc8a149dd89067a99b3fe
iamminseongKim <minseongkimim@gmail.com>
2024-10-12 14:30:31 +0900

    7. 코드 리팩토링 - null넘기는 행위 Optional이용해서 수정하기

--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -9,6 +9,7 @@ import cleancode.studycafe.tobe.model.StudyCafePass;
 import cleancode.studycafe.tobe.model.StudyCafePassType;
 
 import java.util.List;
+import java.util.Optional;
 
 public class StudyCafePassMachine {
 
@@ -23,9 +24,13 @@ public class StudyCafePassMachine {
             outputHandler.showAnnouncement();
 
             StudyCafePass selectedPass = selectPass();
-            StudyCafeLockerPass lockerPass = selectLockerPass(selectedPass);
+            Optional<StudyCafeLockerPass> optionalLockerPass = selectLockerPass(selectedPass);
 
-            outputHandler.showPassOrderSummary(selectedPass, lockerPass);
+            optionalLockerPass.ifPresentOrElse(
+                    lockerPass -> outputHandler.showPassOrderSummary(selectedPass, lockerPass),
+                    () -> outputHandler.showPassOrderSummary(selectedPass)
+            );
+
         } catch (AppException e) {
             outputHandler.showSimpleMessage(e.getMessage());
         } catch (Exception e) {
@@ -51,9 +56,9 @@ public class StudyCafePassMachine {
                 .toList();
     }
 
-    private StudyCafeLockerPass selectLockerPass(StudyCafePass selectedPass) {
+    private Optional<StudyCafeLockerPass> selectLockerPass(StudyCafePass selectedPass) {
         if (selectedPass.getPassType() != StudyCafePassType.FIXED) {
-            return null;
+            return Optional.empty();
         }
 
         StudyCafeLockerPass lockerPassCandidate = findLockerPassCandidateBy(selectedPass);
@@ -63,10 +68,10 @@ public class StudyCafePassMachine {
             boolean isLockerSelected = inputHandler.getLockerSelection();
 
             if (isLockerSelected) {
-                return lockerPassCandidate;
+                return Optional.of(lockerPassCandidate);
             }
         }
-        return null;
+        return Optional.empty();
     }
 
     private StudyCafeLockerPass findLockerPassCandidateBy(StudyCafePass selectedPass) {
